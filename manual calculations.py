57e31eb57036f95198c59451eed9dc4f745b95a5
KAYONGA Elvis <126350069+Elvis-Kayonga@users.noreply.github.com>
2025-11-02 12:56:30 +0200

    Add prediction and MSE computation functions

--- manual calculations.py
+++ manual calculations.py
@@ -9,4 +9,14 @@ def load_data():
     Y = np.array([2, 3, 4], dtype=float)
     return X, Y
 
-    
+# -----------------------------
+# MODULE 2: Prediction function
+# -----------------------------
+def predict(X, m, b):
+    return m * X + b
+
+# -----------------------------
+# MODULE 3: Compute MSE
+# -----------------------------
+def compute_error(Y, Y_pred):
+    return np.mean((Y - Y_pred) ** 2)
